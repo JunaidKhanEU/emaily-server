d9ebf385d10782a54f3712756920115ce18a1aee
junaid khan <junaidkhan1@gmx.com>
2020-01-25 19:26:06 +0000

    users Models

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-
+require('./models/User/User') // provide user schema to create collection in DB for user collection, first it load the schema then use it inside passport config and callback function order of import matters here!!!
 require('./services/passport') // just provide the passport config code before the program need it for google oAuth inside routes
 const authRoutes = require('./routes/authRoutes')
 const { mongoDB } = require('./config/keys.js')
@@ -10,6 +10,7 @@ const { mongoDB } = require('./config/keys.js')
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
   console.log('MongoDB connected')
 })
+
 // middlewares
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,9 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
+const mongoose = require('mongoose')
 
 const keys = require('../config/keys')
+const Users = mongoose.model('users') // import Users Model this way becuase of testing like mocha get issue with normal import from Models from mongoose
 
 passport.use(
   new GoogleStrategy(
@@ -10,10 +12,12 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: 'http://localhost:4000/auth/google/callback'
     },
-    (accessToken, refreshToken, profile, done) => {
-      console.log('accessToken', accessToken)
-      console.log('refreshToken', refreshToken)
-      console.log('profile', profile)
+    async (accessToken, refreshToken, profile, done) => {
+      const newUser = await Users.create({
+        googleId: profile.id
+      })
+
+      console.log(newUser)
     }
   )
 )
