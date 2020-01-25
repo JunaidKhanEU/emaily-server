8851c465cd9014fdb8b295edbf9fa8fbe061993f
junaid khan <junaidkhan1@gmx.com>
2020-01-25 21:52:17 +0000

    cookie-session

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
+
 require('./models/User/User') // provide user schema to create collection in DB for user collection, first it load the schema then use it inside passport config and callback function order of import matters here!!!
 require('./services/passport') // just provide the passport config code before the program need it for google oAuth inside routes
 const authRoutes = require('./routes/authRoutes')
-const { mongoDB } = require('./config/keys.js')
+const { mongoDB, cookieKey } = require('./config/keys.js')
 
 // DB connection
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
@@ -14,6 +17,12 @@ mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }, (
 // middlewares
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(cookieSession({
+  maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in millisecond 30 days, each day 24 h, each hours 60 min , each min 60 seconds, each second has 1000 millisecond
+  keys: [cookieKey]
+})) // tells express to use cookie-session
+app.use(passport.initialize())
+app.use(passport.session()) // connect passport with express and cookie-session
 
 // routes
 app.use(authRoutes)
--- services/passport.js
+++ services/passport.js
@@ -1,9 +1,18 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const mongoose = require('mongoose')
-
 const keys = require('../config/keys')
 const Users = mongoose.model('users') // import Users Model this way becuase of testing like mocha get issue with normal import from Models from mongoose
+
+passport.serializeUser((user, done) => {
+  done(null, user.id) // we select the unique identifier for cookie
+})
+
+// to check cookie unique identifier which we setup earlier
+passport.deserializeUser(async (userId, done) => {
+  const user = await Users.findById(userId)
+  done(null, user)
+})
 
 passport.use(
   new GoogleStrategy(
@@ -13,11 +22,17 @@ passport.use(
       callbackURL: 'http://localhost:4000/auth/google/callback'
     },
     async (accessToken, refreshToken, profile, done) => {
+      const existUser = await Users.findOne({ googleId: profile.id })
+      if (existUser) {
+        console.log(existUser.id)
+        done(null, existUser) // after the done call the passport send second argument(user) to the passport.serializeUser((user, done)=>) to set the cookie using some identifier on user data and later deserialized to check the cookie session.
+      }
+
       const newUser = await Users.create({
         googleId: profile.id
       })
 
-      console.log(newUser)
+      done(null, newUser)
     }
   )
 )
