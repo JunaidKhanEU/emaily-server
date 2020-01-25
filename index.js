21daf59bfac733124390b842b0dc5505f3b282be
junaid khan <junaidkhan1@gmx.com>
2020-01-25 18:40:50 +0000

    DB connected

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-
-
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 
 require('./services/passport') // just provide the passport config code before the program need it for google oAuth inside routes
 const authRoutes = require('./routes/authRoutes')
+const { mongoDB } = require('./config/keys.js')
+
+// DB connection
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+  console.log('MongoDB connected')
+})
 // middlewares
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
